07c8cf55bfab9e5d3dd14ee45afd0faf91e8cc64
Foodi0_0 <volodiafood@gmail.com>
2023-03-07 19:57:41 +0200

    Added bot credentials, changed BOT_TOKEN and name to gets methods

--- src/main/java/temp/telegram/BotConstants.java
+++ src/main/java/temp/telegram/BotConstants.java
@@ -1,7 +1,27 @@
 package temp.telegram;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class BotConstants {
-    public static final String BOT_NAME = "Exchange_java_bot";
-    public static final String BOT_TOKEN = "5835939579:AAHBGZtHEZV3J7QlgplyLc9_1F1Bx7gChhg";
+    private static final String CREDENTIALS_FILE = "config/bot_credentials.txt";
+    private static final Properties properties = new Properties();
+
+    static {
+        try (FileInputStream inputStream = new FileInputStream(CREDENTIALS_FILE)) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private BotConstants(){}
+    public static String getBotToken() {
+        return properties.getProperty("BOT_TOKEN");
+    }
+
+    public static String getBotName() {
+        return properties.getProperty("BOT_NAME");
+    }
 
 }
