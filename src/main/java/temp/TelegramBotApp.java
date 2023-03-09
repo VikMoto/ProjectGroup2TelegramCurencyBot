c11365d756d479c7b3c895111f60e92b6341a443
Roman Kukla <kukla.roman@gmail.com>
2023-03-09 19:06:19 +0200

    Added tests for services

--- src/main/java/temp/TelegramBotApp.java
+++ src/main/java/temp/TelegramBotApp.java
@@ -2,6 +2,7 @@ package temp;
 
 import temp.Api.CurrencyService;
 import temp.Api.MonoCurrencyService;
+import temp.Api.NBUCurrencyService;
 import temp.Api.PrivatBankCurrencyService;
 import temp.currency.dto.Currency;
 import temp.telegram.TelegramBotService;
@@ -16,15 +17,23 @@ public class TelegramBotApp {
     public static void main(String[] args) throws IOException {
         TelegramBotService botService = new TelegramBotService();
 
+        //Privat bank test
         CurrencyService currencyService = new PrivatBankCurrencyService();
         Currency currency = Currency.USD;
         Map<String, BigDecimal> rate = currencyService.getRate(currency);
 
+        //Monobank test
         CurrencyService currencyServiceMono = new MonoCurrencyService();
         Currency currencyMono = Currency.USD;
         Map<String, BigDecimal> rateMono = currencyServiceMono.getRate(currencyMono);
 
+        //NBU test
+        CurrencyService currencyServiceNBU = new NBUCurrencyService();
+        Currency currencyNBU = Currency.USD;
+        Map<String, BigDecimal> rateNBU = currencyServiceNBU.getRate(currencyNBU);
+
         System.out.println("Privat rate = " + rate);
         System.out.println("Monobank rate = " + rateMono);
+        System.out.println("NBU rate = " + rateNBU);
     }
 }
