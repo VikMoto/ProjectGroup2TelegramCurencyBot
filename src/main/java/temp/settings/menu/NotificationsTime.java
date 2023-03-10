56505def234021006464d9e4de771ab0366a9f5b
VikMoto <117093117+VikMoto@users.noreply.github.com>
2023-03-10 23:46:05 +0200

    clean from Unnecessary code

--- src/main/java/temp/settings/menu/NotificationsTime.java
+++ src/main/java/temp/settings/menu/NotificationsTime.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @AllArgsConstructor
