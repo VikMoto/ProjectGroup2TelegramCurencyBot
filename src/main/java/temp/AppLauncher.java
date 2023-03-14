091d99f2026457a1d23385fe3d9fef0aea0810a5
Roman Kukla <kukla.roman@gmail.com>
2023-03-14 15:09:55 +0200

    removed not used exception declaration

--- src/main/java/temp/AppLauncher.java
+++ src/main/java/temp/AppLauncher.java
@@ -1,17 +1,13 @@
 package temp;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.quartz.SchedulerException;
 import temp.schedule.ScheduleService;
 import temp.schedule.impl.ScheduleServiceImpl;
 import temp.telegram.TelegramBotService;
-import java.io.IOException;
 
 public class AppLauncher {
 
-    private static final Logger logger = LogManager.getLogger(AppLauncher.class);
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         TelegramBotService botService = new TelegramBotService();
 
         ScheduleService scheduleService = new ScheduleServiceImpl();
